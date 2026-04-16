bd5d6a8a1d4eefa011e8add2115d3c6afeed4365
cvanshu-dev <cvanshu.dev@gmail.com>
2026-04-16 16:33:00 +0530

    update ddeffh

--- scripts/start.js
+++ scripts/start.js
@@ -1,8 +1,12 @@
 const { execSync } = require("child_process")
 const { existsSync } = require("fs")
 
-if (!existsSync(".medusa/client/index.html")) {
-  execSync("npm run build", { stdio: "inherit" })
+const adminIndex = ".medusa/client/index.html"
+const env = { ...process.env, NPM_CONFIG_PRODUCTION: "false" }
+
+if (!existsSync(adminIndex)) {
+  console.log("Admin build missing, running build...")
+  execSync("npm run build", { stdio: "inherit", env })
 }
 
 execSync("medusa start", { stdio: "inherit" })
